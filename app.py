5e2c938786a7115f020b608cc2285332f0af4d82
cesarverapy <cesar.vera.dev@gmail.com>
2024-12-04 07:12:44 -0300

    fixed missing configurations

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from config import Config
+from routes import register_routes
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
+app.config.from_object(Config)
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+register_routes(app)
+
+if __name__ == '__main__':
     app.run(debug=True)
